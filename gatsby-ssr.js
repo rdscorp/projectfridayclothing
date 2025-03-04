7fb410f828724f3f8e00c276a42212b3b7513b27
RDS Corp <rajputanadataservices@gmail.com>
2025-03-04 18:21:35 +0530

    ver-1.0.5 rdt-4/3/25|18:21

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -7,6 +7,9 @@
 /**
  * @type {import('gatsby').GatsbySSR['onRenderBody']}
  */
+import "./src/components/index.module.css"; // Adjust the path if necessary
+import "./src/components/layout.css"; // Adjust the path if necessary
+
 exports.onRenderBody = ({ setHtmlAttributes }) => {
   setHtmlAttributes({ lang: `en` })
 }
